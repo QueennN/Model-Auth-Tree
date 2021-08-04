32789b7e0ae2dc1a39b473ef4e47de5a96e56655
umudikk <seyitumutbicer@gmail.com>
2021-08-04 17:16:42 +0300

    dev

--- src/life-cycle/preRule.js
+++ src/life-cycle/preRule.js
@@ -1,4 +1,11 @@
 module.exports = async function (payload, ctx) {
+   if (ctx.local.has("model", payload.model)) {
+      let model = ctx.local.get("model", payload.model)
+      if (!model.methods.has(payload.method)) return false
+   } else {
+      return false
+   } // TODO BU yöntemden kurtul.
+
    let rules = ctx.helpers.defaultArrayCalc(payload, "preRule");
    if (rules.every((rule) => ctx.local.has("rule", rule))) {
       for (let rule of rules) {
@@ -12,7 +19,7 @@ module.exports = async function (payload, ctx) {
       }
       return true;
    } else {
-      payload.response.warnings.push(`Missing preRule`, ctx.lodash.remove(rules, r => ctx.local.has("rule",r)));
+      payload.response.warnings.push(`Missing preRule`, ctx.lodash.remove(rules, r => ctx.local.has("rule", r)));
       return false
    }
 };
